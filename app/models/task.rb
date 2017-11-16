4de18a39da85aefd0911283082c54134b9941d74
Dany Marcoux <danymarcoux@gmail.com>
2017-11-16 20:17:47 +0100

    Validate tasks

--- app/models/task.rb
+++ app/models/task.rb
@@ -1,5 +1,4 @@
 class Task < ApplicationRecord
-  # Each user has his/her tasks in a separate database, so we connect to it
-  # TODO: Setup authentication, then uncomment this
-  # Database.connect_to(Database.name(current_user.id))
+  validates_presence_of :start, :end, :description
+  # TODO: Validate dates start < end
 end
