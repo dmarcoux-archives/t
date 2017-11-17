4c0690b6bdcb7d0bca69c401c4c14210200657b1
Dany Marcoux <danymarcoux@gmail.com>
2017-11-17 16:28:38 +0100

    Validate dates of tasks

--- app/models/task.rb
+++ app/models/task.rb
@@ -1,4 +1,14 @@
 class Task < ApplicationRecord
   validates_presence_of :start, :end, :description
-  # TODO: Validate dates start < end
+  validate :validate_start
+  validate :validate_end
+
+  def validate_start
+    errors.add(:start, 'must be in the future') if start < Time.now
+    errors.add(:start, 'must be before the end date') if start >= self.end
+  end
+
+  def validate_end
+    errors.add(:end, 'must be in the future') if self.end < Time.now
+  end
 end
